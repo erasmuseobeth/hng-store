51ec866284b155b7281b9dae6145a402e349f2fe
cyber3330d <97829437+cyber330d@users.noreply.github.com>
2024-07-09 11:49:38 +0100

    Update BenefitsCard.tsx

--- app/components/BenefitsCard.tsx
+++ app/components/BenefitsCard.tsx
@@ -1,24 +1,9 @@
-import { Guarantee, ShipCar } from "./Icons";
+
 
 export default function Home() {
     return (
       <div>
-        <div>
-{/*           <MonitorLine/> */}
-          <span>Latest and Greatest Tech</span>
-        </div>
-        <div>
-{/*           <GuaranteeLine/> */}
-          <span>Guarantee</span>
-        </div>
-        <div className="gap-3">
-{/*           <ShipCarLine/> */}
-          <span>Free Shipping over 1000$</span>
-        </div>
-        <div>
-{/*         <ClockLine24/> */}
-          <span>24/7 Support</span>
-        </div>
+      ggg
         </div>
     );
   }

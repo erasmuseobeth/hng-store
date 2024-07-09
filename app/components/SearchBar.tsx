ef7dfe135565981c9bdd40f767867e00d6e74bbe
cyber330d <cyber330d@gmail.com>
2024-07-09 16:28:25 +0100

    fdd

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -4,7 +4,7 @@ import { Search } from './Icons';
 
 export default function SearchBar() {
   return ( 
-    <div className="flex items-center justify-between self-stretch px-6 rounded-lg box-border shadow-md mx-3 min-h-9 py-1 mt-16 mb-4 has-[:focus]:ring-1 ring-black- ring-1">
+    <div className="flex items-center justify-between self-stretch px-6 rounded-lg box-border shadow-md border mx-3 min-h-9 py-1 mt-16 mb-4 has-[:focus]:ring-1">
 
     <input type="search" id='search' placeholder='What can we help you to find ?' className='py-2 bg-transparent outline-none text-red text-[16px] text-[#1E5EFF]'/>
     <label htmlFor="search">
